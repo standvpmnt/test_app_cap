f914c0bbdba607ac613744cbb00c74891ae280de
STan Dvpmnt <standvpmnt@gmail.com>
2020-02-14 17:24:43 +0530

    Trying git with capistrano

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+TaskList.create(task: "Check if seeding works with existing capistrano seetup", priority: 6)
+TaskList.create(task: "Verify if this works with out a push to github (unlikely as it may sound", priority: 4)
+TaskList.create(task: "Get this running right away and delete this one if all goes well", priority: 7)
+TaskList.create(task: "Now we are good to go with Capistrano and deploying our app to AWS!", priority: 8)
+TaskList.create(task: "Start the deployment process and keep this instance until you are done", priority: 9)
