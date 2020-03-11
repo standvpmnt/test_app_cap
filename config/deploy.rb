64b7e452ad6fbeec9f613793c9485c2a85c18784
STan Dvpmnt <standvpmnt@gmail.com>
2020-03-11 16:03:17 +0530

    Sidekiq error

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,3 +40,5 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 set :puma_init_active_record, true
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+SSHKit.config.command_map[:sidekiq] = "bundle exec sidekiq"
+SSHKit.config.command_map[:sidekiqctl] = "bundle exec sidekiqctl"
