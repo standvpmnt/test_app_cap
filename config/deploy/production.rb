d6a9704f58ee7d9a55e7590a158cbf94f61a885f
STan Dvpmnt <standvpmnt@gmail.com>
2020-03-11 16:21:09 +0530

    Loading config of sidekiq in files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,19 @@ server "13.233.195.147", user: "ubuntu", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+:sidekiq_default_hooks =>  true
+:sidekiq_pid =>  File.join(shared_path, 'tmp', 'pids', 'sidekiq.pid')
+:sidekiq_env =>  fetch(:rack_env, fetch(:rails_env, fetch(:stage)))
+:sidekiq_log =>  File.join(shared_path, 'log', 'sidekiq.log')
+:sidekiq_options =>  nil
+:sidekiq_require => nil
+:sidekiq_tag => nil
+:sidekiq_config => nil
+:sidekiq_queue => nil
+:sidekiq_timeout =>  10
+:sidekiq_role =>  :app
+:sidekiq_processes =>  1
+:sidekiq_concurrency => nil
 
 # role-based syntax
 # ==================
